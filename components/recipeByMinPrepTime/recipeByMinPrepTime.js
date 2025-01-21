c9d94c478940407a781bccefe4b1d35165fff93a
etomiello <eftomiello@gmail.com>
2025-01-21 10:18:26 -0500

    user story 7, made quick addition with searchbar

--- components/recipeByMinPrepTime/recipeByMinPrepTime.js
+++ components/recipeByMinPrepTime/recipeByMinPrepTime.js
@@ -4,15 +4,30 @@ const apiKey7 ='bf2d548141f94d67bcc99b158ade286a'
 angular.module('prepTime', [])
 .component('prepTime', {
     templateUrl: 'components/recipeByMinPrepTime/recipeByMinPrepTime.html',
-    controller: function($scope, $http) {
+    controller: function($scope, $http, searchBarService) {
 
         $scope.selectedTime = '';
         $scope.listedRecipes = [];
 
+        $scope.newQuery = ''
+        
+
+        $scope.$watch(
+            function () {
+              return searchBarService.query; // Observe the query property
+            },
+            function (newQuery) {
+              if (newQuery) {
+                console.log('Updated Query:', newQuery);
+                $scope.newQuery = newQuery;
+              }
+            }
+          );
+
         $scope.handleTimeSearch = function() {
             const maxTime = parseInt($scope.selectedTime, 10); //convert to int
         
-            $http.get(`https://api.spoonacular.com/recipes/complexSearch?maxReadyTime=${maxTime}&number=1&addRecipeInformation=true&apiKey=${apiKey7}`)
+            $http.get(`https://api.spoonacular.com/recipes/complexSearch?query=${$scope.newQuery}&maxReadyTime=${maxTime}&number=1&addRecipeInformation=true&apiKey=${apiKey7}`)
                 .then((response) => {
                     console.log('API Response:', response.data.results);
                     $scope.listedRecipes = response.data.results.filter(function(recipe) {
