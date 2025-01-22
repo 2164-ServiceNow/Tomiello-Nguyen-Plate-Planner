9fd75891b829f4c5c421a6ee0024c78b0c153631
etomiello <eftomiello@gmail.com>
2025-01-22 16:36:09 -0500

    added styles to cusine page

--- components/recipeByCusines/recipeByCusines.js
+++ components/recipeByCusines/recipeByCusines.js
@@ -1,4 +1,5 @@
-const apiKey5='bf2d548141f94d67bcc99b158ade286a'
+//const apiKey5='bf2d548141f94d67bcc99b158ade286a'
+const apiKey5='d3476fe6c2644d64ba5f7973ed2fb875'
 
 
 angular.module('recipeByCusines', []) 
@@ -10,7 +11,7 @@ angular.module('recipeByCusines', [])
         $scope.listRecipes = [];
 
         $scope.handleCuisineSearch = function() {
-            $http.get(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${$scope.selectedCuisine}&number=1&addRecipeInformation=true&apiKey=${apiKey5}`)
+            $http.get(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${$scope.selectedCuisine}&number=2&addRecipeInformation=true&apiKey=${apiKey5}`)
             .then((response) => {
                 console.log('Recipes retrieved:', response.data);
                 $scope.listRecipes = response.data.results
