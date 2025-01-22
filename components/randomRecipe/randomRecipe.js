0939ffc8ef7d3c163bbea2575b6075ffa1626f87
davidnguyenc1 <davidnguyenc1>
2025-01-22 07:35:31 -0800

    started on user story 8, took out drop box for now since i cant get it to work, only has a button to output the values and fixed instructions to look more readable

--- components/randomRecipe/randomRecipe.js
+++ components/randomRecipe/randomRecipe.js
@@ -1,38 +1,29 @@
 //const apiKey8='d3476fe6c2644d64ba5f7973ed2fb875'
-const apiKey8='b7fad37df2234ebdbaadb879ae6c6a61'
-//const apiKey8='bf2d548141f94d67bcc99b158ade286a'
+// const apiKey8='b7fad37df2234ebdbaadb879ae6c6a61'
+const apiKey8='bf2d548141f94d67bcc99b158ade286a'
 
 angular.module('randomRecipe', [])
 
 .component('randomRecipe',{
     templateUrl: 'components/randomRecipe/randomRecipe.html',
     controller: function($scope, $http){
-        
-        // List of meal types
-        $scope.dishTypes = ['Breakfast', 'Appetizer', 'Main Course', 'Snack', 'Dessert', 
-                            'Soup', 'Bread', 'Salad', 'Side Dish'];
-        $scope.selectedDishType = '';
 
-        $scope.getRandomRecipe = function() {
+        $scope.selectedDishType = ''
+        $scope.recipe = ''
 
-            let apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey8}&number=1`;
-
-            // Add selected dish types if available
-            if ($scope.selectedDishType) {
-                apiUrl += '&include-tags=' + $scope.selectedDishType;
-            }
-
-            // Include nutrition information
-            apiUrl += '&includeNutrition=true';
+        $scope.searchRandomRecipes = function() {
+            const dishParam = $scope.selectedDishType;
 
             // Make API request to fetch random recipe
-            $http.get(apiUrl)
-                .then(function(response) {
-                $scope.recipe = response.data.recipes[0];
+            $http.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey8}&include-tags=${dishParam}&number=1&includeNutrition=true`)
+                .then((response) => {
+                $scope.recipe = response.data.recipes[0]
+                console.log(response.data.recipes[0])
             });
-        };
+        }
+        
 
-        $scope.getRandomRecipe(); // on page load
+        $scope.searchRandomRecipes(); // on page load
                 
         }
     }
