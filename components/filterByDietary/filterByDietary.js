3dfb444bda2f73d6e447cd65c08e4683aa85c86b
davidnguyenc1 <davidnguyenc1>
2025-01-22 13:43:05 -0800

    added styling to dietary user story

--- components/filterByDietary/filterByDietary.js
+++ components/filterByDietary/filterByDietary.js
@@ -1,6 +1,6 @@
-//const apiKey='d3476fe6c2644d64ba5f7973ed2fb875'
-//const apiKey='b7fad37df2234ebdbaadb879ae6c6a61'
-const apiKey2='bf2d548141f94d67bcc99b158ade286a'
+const apiKey2='d3476fe6c2644d64ba5f7973ed2fb875'
+//const apiKey2='b7fad37df2234ebdbaadb879ae6c6a61'
+//const apiKey2='bf2d548141f94d67bcc99b158ade286a'
 
 angular.module('filterByDietary', [])
 
