b86a32c1f6a6c45c5a086540ec2274fc28eb87bf
etomiello <eftomiello@gmail.com>
2025-01-23 15:11:14 -0500

    fixed search bar error in cusine

--- components/recipeByCusines/recipeByCusines.js
+++ components/recipeByCusines/recipeByCusines.js
@@ -1,22 +1,24 @@
 const apiKey5='b7fad37df2234ebdbaadb879ae6c6a61'
 
 
-angular.module('recipeByCusines', []) 
+angular.module('recipeByCusines', [])
 .component('recipeByCusines', {
     templateUrl: 'components/recipeByCusines/recipeByCusines.html', 
-    controller: function($scope, $http){ 
+    controller: function($scope, $http, searchBarService) {
         
         $scope.selectedCuisine = '';
         $scope.listRecipes = [];
 
         $scope.handleCuisineSearch = function() {
-            $http.get(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${$scope.selectedCuisine}&number=4&addRecipeInformation=true&apiKey=${apiKey5}`)
+            const searchTerm = searchBarService.getQuery(); 
+            $http.get(`https://api.spoonacular.com/recipes/complexSearch?cuisine=${$scope.selectedCuisine}&query=${searchTerm}&number=4&addRecipeInformation=true&apiKey=${apiKey5}`)
             .then((response) => {
                 console.log('Recipes retrieved:', response.data);
-                $scope.listRecipes = response.data.results
+                $scope.listRecipes = response.data.results;
             })
-        }
-        
-        
+            .catch((error) => {
+                console.error('Error fetching recipes:', error);
+            });
+        };
     }
 });
